481b7232bc992d071cadcf3e41d33279e3e9e799
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-17 21:31:34 +0530

    Add files via upload

---
+++ Array class/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Array_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list = { 34, 72, 13, 44, 25, 30, 10 };
+            int[] temp = list;
+            Console.Write("Original Array: ");
+            foreach (int i in list)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            Array.Reverse(temp);                            //To Reverse Array
+            Console.Write("Reverse Array: ");
+            foreach (int i in temp)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            Array.Sort(list);                               //To Sort Array
+            Console.Write("Sorted Array: ");
+            foreach (int i in list)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
