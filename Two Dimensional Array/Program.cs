3b5ab2083f22a2073cab2b983833b3a4a414f2d4
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-17 21:26:22 +0530

    Add files via upload

---
+++ Two Dimensional Array/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TwoDimensional_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numArray = new int[3, 4] { { 0, 1, 2, 3 }, { 4, 5, 6, 7 }, { 8, 9, 10, 11 } };
+            //int val = numArray[2, 3];           //Accessing individual Elements
+            //Console.WriteLine(val);
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j <4 ; j++)
+                {
+                    Console.WriteLine("numArray[{0},{1}] = {2}", i, j, numArray[i, j]);
+                }
+            }
+        }
+    }
+}
