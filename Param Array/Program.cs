e3209e3a3262d578484849e46bd91bf419c1582c
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-17 21:29:45 +0530

    Add files via upload

---
+++ Param Array/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Param_Arrays
+{
+    class Program
+    {
+        public static void UseParams(params int[] list)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                Console.Write(list[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            UseParams(1, 2, 3, 4);
+            UseParams(1, 2, 3);
+            int[] intArray = { 5, 6, 7, 8, 9 };
+            UseParams(intArray);
+        }
+    }
+}
